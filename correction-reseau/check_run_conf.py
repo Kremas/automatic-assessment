1f7a6eda4a49140561af7f7595d48aeb80d11038
Antoine GRANDJACQUOT <antoine.gj@gmail.com>
2019-02-11 10:09:59 +0100

    improved cisco conf automatic assessment

--- correction-reseau/check_run_conf.py
+++ correction-reseau/check_run_conf.py
@@ -20,8 +20,16 @@ class C(object):
                 print(elem.tag, elem.text)
             print("")
 
+    def check_misc(self, node):
+        res = True
+        for elem in node:
+            if(elem.tag == 'motif'):
+                if(len(self.conf.find_objects(elem.text)) == 0):
+                   res = False
+        return res
+
     def check_node(self, parent, node):
-        result = node.find('result').text
+        result = node.find('parent').text
         l_result = []
         for elem in node:
             if(elem.tag == 'motif'):
@@ -39,16 +47,34 @@ class C(object):
         for child in self.root:
             for elem in child:
                 if(elem.tag == 'type'):
+                    if(elem.text == 'misc'):
+                        if(self.check_misc(child)):
+                            points += int(child.find('points').text)
                     if(elem.text == 'interface configuration'):
                         if(self.check_node('interface', child)):
                             points += int(child.find('points').text)
                     if(elem.text == 'ospf configuration'):
                         if(self.check_node('router ospf', child)):
                             points += int(child.find('points').text)
+                    if(elem.text == 'isis configuration'):
+                        if(self.check_node('router isis', child)):
+                            points += int(child.find('points').text)
+                    if(elem.text == 'eigrp configuration'):
+                        if(self.check_node('router eigrp', child)):
+                            points += int(child.find('points').text)
+                    if(elem.text == 'rip configuration'):
+                        if(self.check_node('router rip', child)):
+                            points += int(child.find('points').text)
+                    if(elem.text == 'bgp configuration'):
+                        if(self.check_node('router bgp', child)):
+                            points += int(child.find('points').text)
+                    if(elem.text == 'line configuration'):
+                        if(self.check_node('line', child)):
+                            points += int(child.find('points').text)
 
         return points
 
 if __name__ == '__main__':
-    obj = C('check_run_conf.xml', 'R2.txt')
+    obj = C('check_run_conf.xml', 'S2.txt')
     obj.print_xml()
     print(obj.assess())
